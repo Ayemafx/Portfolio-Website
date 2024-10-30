d1a7055c7e9335bef60a4f310e94db233d1a664a
Ayemafx <aaimafaisal2610@gmail.com>
2024-10-30 11:57:05 +0500

    fixed bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+      <div className="w-full absolute left-0 bottom-0 min-h-96">
         <img
           src="/footer-grid.svg"
           alt="grid"
@@ -45,12 +45,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img
-                src={info.img}
-                alt="icons"
-                width={20}
-                height={20}
-              />
+              <img src={info.img} alt="icons" width={20} height={20} />
             </Link>
           ))}
         </div>
